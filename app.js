68612a718376b33c71262d14f2de2b9a6d908bcd
zarnigor <zarnigorasatova52@gmail.com>
2024-04-03 16:48:13 +0500

    feat: feature

---
+++ app.js
@@ -0,0 +1,29 @@
+// 1 task
+
+const CostNumber = 10;
+if (CostNumber % 3 === 0) {
+    console.log("fizz");
+} else if (CostNumber % 5 === 0) {
+    console.log("buzz");
+} else if (CostNumber % 5 === 0 && CostNumber % 5 === 0) {
+    console.log("Fizzbuzz");
+} else {
+    console.log(CostNumber);
+}
+
+// 2 task
+
+function buManzara(width, height) {
+    let result;
+    if (width > height) {
+        result = "Ha Bu Manzara";
+    } else {
+        result = "Yoq Bu Manzara emas";
+    }
+    return result;
+}
+console.log(buManzara(500, 100));
+
+// 3- task
+
+console.log(Math.max(3, 2));
